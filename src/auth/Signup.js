277aca671fba2ea36dd4b4988c0bab5b74739eae
Mohammad Laraib <104249752+iamLaraib@users.noreply.github.com>
2024-04-05 23:33:10 +0530

    give a ride

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -306,8 +306,8 @@ const Signup = () => {
                 </div>
               </div>
 
-              <div className="mt-6 grid grid-cols-3 gap-3">
-                <div>
+              <div className="mt-6">
+                {/* <div>
                   <a
                     href="#"
                     className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
@@ -330,7 +330,7 @@ const Signup = () => {
                       alt=""
                     />
                   </a>
-                </div>
+                </div> */}
                 <div>
                   <a
                     onClick={handleGoogleClick}
